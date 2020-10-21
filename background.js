be7cf27882010f8b57fcffcbe4a63a176a5ec1c4
Spühler Stefan <git@tuxli.ch>
2020-10-21 13:23:43 +0200

    Only send a request to the API if the popup window is opened
    This reduces the number of requests / bandwidth requirements and reduces
    the privacy impact.

--- background.js
+++ background.js
@@ -11,61 +11,26 @@ function addIPtoTitle(responseDetails) {
 
 	if (tabId){		
 		if (! ipData[tabId])  ipData[tabId] = {} ;
-		if (! asn[tabId])  asn[tabId] = {} ;
-		if (! prefix[tabId]) prefix[tabId] = {};
-		if (! processedIps[tabId]) processedIps[tabId] = {};
 
 		if(responseDetails.type === "main_frame"){
-			asn[tabId] = {} ;
 			ipData[tabId] = {};
-			prefix[tabId] = {};
-			ipData[tabId] = {} ;
-			processedIps[tabId] = {} ;
 		}
 
-		if (!(tabId === -1) && (!(responseDetails.ip in processedIps[tabId])) && responseDetails.ip) {
-		      processedIps[tabId][responseDetails.ip] = true;
-			  const requestURL = "https://asnumber.tuxli.ch/asnumber/asnum?ip=" + responseDetails.ip;
-			  const driveRequest = new Request(requestURL, {
-				method: "GET"
-			  });
-			  fetch(driveRequest)
-				.then(function(response) { return response.json(); })
-				.then(function(json) {
-					asn[tabId][json.asn] = {
-						"asn" : json.asn,
-						"prefixes" : json.prefixes,
-						"asname" : json.asname,
-						"asdesc" : json.asdesc,
-						"country" : json.country,
-						"rir" : json.rir,
-					};
-					if (! prefix[tabId][json.asn]) {
-						prefix[tabId][json.asn] = [json.prefix]
-					}
-					else {
-						if (! prefix[tabId][json.asn].includes(json.prefix))
-						prefix[tabId][json.asn].push(json.prefix)
-					}
-					processedIps[tabId][responseDetails.ip] = json.asn;
-					add_ips(responseDetails, processedIps, tabId);
-				});
-		}
-		else if (tabId !== -1 && responseDetails.ip) {
-			add_ips(responseDetails, processedIps, tabId);
-		}
+		if (tabId !== -1 && responseDetails.ip) {
+					add_ips(responseDetails, tabId);
+				}
 
-  }
-}
-function add_ips(responseDetails, processedIps, tabId) {
+		  }
+		}
+function add_ips(responseDetails, tabId) {
 	let parser;
 	if (responseDetails.ip) { // not from cache
 		parser = document.createElement('a');
 		parser.href = responseDetails.url;
 		let hostname = parser.hostname
 		let ip = responseDetails.ip;
-		if (!ipData[tabId][ip]) ipData[tabId][ip] = {};
-		if (!ipData[tabId][ip][ip]) {
+		//if (!ipData[tabId][ip]) ipData[tabId][ip] = {};
+		if (!ipData[tabId][ip]) {
 			ipData[tabId][ip] = {
 				"type": [responseDetails.type],
 				"hostname": hostname
@@ -79,7 +44,7 @@ function add_ips(responseDetails, processedIps, tabId) {
 }
 
 // define variables
-var ipData ={}; var asn ={}; var prefix = {}; var processedIps = {};
+var ipData = {};
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
 	
--- popup.js
+++ popup.js
@@ -1,95 +1,123 @@
-function nsGhipUtil_load(){
-	
-	let tabId = parseInt(window.location.hash.replace('#','')) ;
-	
-	let asn = chrome.extension.getBackgroundPage().asn[tabId]  ;
+const nsGhipUtil_load = async () => {
+
+    const tabId = parseInt(window.location.hash.replace('#', ''));
+
+    let ipData = chrome.extension.getBackgroundPage().ipData[tabId];
+    let asn = await getAsnDetails(ipData);
+    console.log(asn);
+    console.log(Object.keys(asn))
+
+    for (let i in asn) {
+        let a = document.createElement('a');
+        let br = document.createElement('br');
+        a.href = 'https://bgp.he.net/AS' + asn[i].asn;
+        a.text = "AS" + asn[i].asn + ' ' + asn[i].asname;
+        let im = document.createElement('img');
+        im.src = "icons/clipboard.png";
+        im.data = {asn: asn[i].asn};
+        im.className = "copy";
+        im.onclick = copyToClipboard;
+        document.querySelector('#asn').appendChild(im);
+        document.querySelector('#asn').appendChild(a);
+        document.querySelector('#asn').appendChild(br);
+    }
+
+    for (let i in asn) {
+        let docFragment = document.createDocumentFragment();
+        let text = document.createTextNode("AS: " + asn[i].asn);
+        docFragment.appendChild(text);
+
+        let br = document.createElement('BR');
+        docFragment.appendChild(br);
+        let text_0 = document.createTextNode("Prefixes: " + asn[i].prefixes);
+        docFragment.appendChild(text_0);
+
+        let br_0 = document.createElement('BR');
+        docFragment.appendChild(br_0);
+        let text_1 = document.createTextNode("AS name: " + asn[i].asname);
+        docFragment.appendChild(text_1);
+
+        let br_1 = document.createElement('BR');
+        docFragment.appendChild(br_1);
+        let text_2 = document.createTextNode("AS desc: " + asn[i].asdesc);
+        docFragment.appendChild(text_2);
+
+        let br_2 = document.createElement('BR');
+        docFragment.appendChild(br_2);
+        let text_3 = document.createTextNode("Country: " + asn[i].country);
+        docFragment.appendChild(text_3);
+
+        let br_3 = document.createElement('BR');
+        docFragment.appendChild(br_3);
+        let text_4 = document.createTextNode("RIR: " + asn[i].rir);
+        docFragment.appendChild(text_4);
+
+        let br_4 = document.createElement('BR');
+        docFragment.appendChild(br_4);
+        for (let p in [asn[i].prefix]) {
+            let text_5 = document.createTextNode("Prefix: " + asn[i].prefix[p]);
+            docFragment.appendChild(text_5);
+            let br_5 = document.createElement('BR');
+            docFragment.appendChild(br_5);
+        }
+        let br_6 = document.createElement('BR');
+        docFragment.appendChild(br_6);
+        let br_7 = document.createElement('div')
+        docFragment.appendChild(br_7);
+
+        document.querySelector('#asns').appendChild(docFragment);
+
+    }
+    for (let count in ipData) {
+        let a = document.createElement('div');
+        let text_node = document.createTextNode(ipData[count].hostname + ' (' + ipData[count]['type'].join(", ") + ') : ' + count);
+        a.appendChild(text_node)
+        document.querySelector('#ips').appendChild(a);
+    }
 
 
-	for (let i in asn ){
-		let a = document.createElement('a');
-		let br = document.createElement('br');
-		a.href = 'https://bgp.he.net/AS'+asn[i].asn;
-		a.text = "AS" + asn[i].asn + ' ' + asn[i].asname;
-		let im = document.createElement('img');
-		im.src = "icons/clipboard.png";
-		im.data = {asn : asn[i].asn};
-		im.className="copy";
-		im.onclick = copyToClipboard ;
-		document.querySelector('#asn').appendChild(im);
-		document.querySelector('#asn').appendChild(a);
-		document.querySelector('#asn').appendChild(br);
-	}
-	
-	
-	let ips = chrome.extension.getBackgroundPage().ipData[tabId]  ;
-	let prefix = chrome.extension.getBackgroundPage().prefix[tabId]  ;
-	for (let i in asn ){
-		let docFragment = document.createDocumentFragment();
-		let text = document.createTextNode("AS: " + asn[i].asn);
-		docFragment.appendChild(text);
-
-		let br = document.createElement('BR');
-		docFragment.appendChild(br);
-		let text_0 = document.createTextNode("Prefixes: " + asn[i].prefixes);
-		docFragment.appendChild(text_0);
-
-		let br_0 = document.createElement('BR');
-		docFragment.appendChild(br_0);
-		let text_1 = document.createTextNode("AS name: " + asn[i].asname);
-		docFragment.appendChild(text_1);
-
-		let br_1 = document.createElement('BR');
-		docFragment.appendChild(br_1);
-		let text_2 = document.createTextNode("AS desc: " + asn[i].asdesc);
-		docFragment.appendChild(text_2);
-
-		let br_2 = document.createElement('BR');
-		docFragment.appendChild(br_2);
-		let text_3 = document.createTextNode("Country: " + asn[i].country);
-		docFragment.appendChild(text_3);
-
-		let br_3 = document.createElement('BR');
-		docFragment.appendChild(br_3);
-		let text_4 = document.createTextNode("RIR: " + asn[i].rir);
-		docFragment.appendChild(text_4);
-
-		let br_4 = document.createElement('BR');
-		docFragment.appendChild(br_4);
-		for (let p in prefix[asn[i].asn]){
-			let text_5 = document.createTextNode("Prefix: " + prefix[asn[i].asn][p]);
-			docFragment.appendChild(text_5);
-			let br_5 = document.createElement('BR');
-			docFragment.appendChild(br_5);
-		}
-		let br_6 = document.createElement('BR');
-		docFragment.appendChild(br_6);
-		let br_7 = document.createElement('div')
-		docFragment.appendChild(br_7);
-
-		document.querySelector('#asns').appendChild(docFragment);
-		for (let count in ips ){
-			let a = document.createElement('div');
-			let text_node = document.createTextNode(ips[count].hostname + ' ('+ips[count]['type'].join(", ")+') : ' + count);
-			a.appendChild(text_node)
-			document.querySelector('#ips').appendChild(a);
-		}
-	}
-	
 }
-window.addEventListener("load", nsGhipUtil_load ,false);
 
-function copyToClipboard(){
-	let copyFrom = document.createElement("textarea");
-	  copyFrom.textContent = this.data.asn;
-	  let body = document.getElementsByTagName('body')[0];
-	  body.appendChild(copyFrom);
-	  copyFrom.select();
-	  document.execCommand('copy');
-	  body.removeChild(copyFrom);  
-	  this.style.border= "1px solid red";
-	  let that  = this;
-	  setTimeout(function(){
-		  that.style.border = 'none';
-	  },1000);
+async function getAsnDetails(ipData) {
+    let asn = {};
+    for (let ip in ipData) {
+        const requestURL = "https://asnumber.tuxli.ch/asnumber/asnum?ip=" + ip;
+        const response = await fetch(requestURL);
+        const json = await response.json();
+        asn[json.asn] = {
+            "asn": json.asn,
+            "prefixes": json.prefixes,
+            "asname": json.asname,
+            "asdesc": json.asdesc,
+            "country": json.country,
+            "rir": json.rir,
+        };
+
+        if (!asn[json.asn]['prefix']) {
+            asn[json.asn]['prefix'] = [json.prefix]
+        } else {
+            if (!asn[json.asn]['prefix'].includes(json.prefix))
+                asn[json.asn]['prefix'].push(json.prefix)
+        }
+    }
+    return asn;
+}
+
+window.addEventListener("load", nsGhipUtil_load, false);
+
+
+function copyToClipboard() {
+    let copyFrom = document.createElement("textarea");
+    copyFrom.textContent = this.data.asn;
+    let body = document.getElementsByTagName('body')[0];
+    body.appendChild(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    body.removeChild(copyFrom);
+    this.style.border = "1px solid red";
+    let that = this;
+    setTimeout(function () {
+        that.style.border = 'none';
+    }, 1000);
 }
 
